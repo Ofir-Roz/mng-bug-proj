55a8795d29386bd51588afe3ab1c076cd6b35a99
Ofir-Roz <ofir19923@gmail.com>
2025-04-15 13:57:59 +0300

    Add bug management functionality and utilities
    - Implement CRUD operations for bugs in server.js
    - Create bug service with query, getById, save, and remove methods
    - Add utility functions for reading and writing JSON files
    - Introduce new data files: bugs.json and bugs-backup.json
    - Update package.json to include development script

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
 
 const app = express()
 
@@ -6,7 +7,56 @@ app.get('/', (req, res) => { res.send('Hello there!') })
 app.listen(3030, () => { console.log('Server ready at port 3030') })
 
 
-app.get('/api/bug', async (req, res) => { })
-app.get('/api/bug/save', async (req, res) => { })
-app.get('/api/bug/:bugId', async (req, res) => { })
-app.get('/api/bug/:bugId/remove', async (req, res) => { })
+//* ------------------------------ Bug Crud API ------------------------------ *//
+//* List
+
+app.get('/api/bug', async (req, res) => {
+    try {
+        const bugs = await bugService.query()
+        res.send(bugs)
+    } catch (err) {
+        console.log('err:', err)
+        res.status(400).send('Failed to get bugs')
+    }
+})
+
+//* Create/Update
+app.get('/api/bug/save', async (req, res) => {
+    const bugToSave = {
+        _id: req.query._id ,
+        title: req.query.title,
+        severity: +req.query.severity,
+        createdAt: +Date.now()
+    }
+    try {
+        const savedBug = await bugService.save(bugToSave)
+        res.send(savedBug)
+    } catch (err) {
+        console.log('err:', err)
+        res.status(400).send('Failed to save bug')
+    }
+})
+
+//* Read
+app.get('/api/bug/:bugId', async (req, res) => { 
+    const { bugId } = req.params
+    try {
+        const bug = await bugService.getById(bugId)
+        res.send(bug)
+    } catch (err) {
+        console.log('err:', err)
+        res.status(400).send('Failed to get bug')
+    }
+})
+
+ //* Delete
+app.get('/api/bug/:bugId/remove', async (req, res) => { 
+    const { bugId } = req.params
+    try {
+        await bugService.remove(bugId)
+        res.send('Bug removed')
+    } catch (err) {
+        console.log('err:', err)
+        res.status(400).send('Failed to remove bug')
+    }
+})
--- services/bug.service.js
+++ services/bug.service.js
@@ -1,6 +1,4 @@
-
-
-const STORAGE_KEY = 'bugDB'
+import { makeId, readJsonFile, writeJsonFile } from "./utils.js"
 
 export const bugService = {
     query,
@@ -9,21 +7,57 @@ export const bugService = {
     remove,
 }
 
-function query() {}
-function getById() {}
-function remove() {}
-function save() {}
+const bugs = readJsonFile('./data/bugs.json')
 
-// Private functions
-function _save(entityType, entities) {
-    localStorage.setItem(entityType, JSON.stringify(entities))
+async function query() {
+    try {
+        return bugs
+    } catch (err) {
+        throw err
+    }
 }
 
-function _makeId(length = 5) {
-    var text = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
+async function getById(bugId) {
+    try {
+        const bug = bugs.find(bug => bug._id === bugId)
+        if (!bug) throw new Error('Bug not found')
+        return bug
+    } catch (err) {
+        throw err
     }
-    return text
+}
+
+async function remove(bugId) {
+    try {
+        const bugIdx = bugs.findIndex(bug => bug._id === bugId)
+        if (bugIdx === -1) throw new Error('Bug not found')
+        bugs.splice(bugIdx, 1)
+        await _saveBugsToFile()
+    } catch (err) {
+        throw err
+    }
+    
+}
+
+async function save(bugToSave) {
+    try {
+        if (bugToSave._id) {
+            const bugIdx = bugs.findIndex(bug => bug._id === bugToSave._id)
+            if (bugIdx === -1) throw new Error('Bug not found')
+            bugs[bugIdx] = bugToSave
+        }
+        else {
+            bugToSave._id = makeId()
+            bugs.unshift(bugToSave)
+        }
+        await _saveBugsToFile()
+        return bugToSave
+    } catch (err) {
+        throw err
+    }
+}
+
+// Private functions
+function _saveBugsToFile() {
+    return writeJsonFile('./data/bugs.json', bugs)
 }
